8e2263e83f0df7763c2040cb0755dff6dbd93284
johnny <jhuang007007@gmail.com>
2022-02-23 14:08:21 -0500

    completed exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  //(F-32) * 5/9
+  let convertedTemp = (temp - 32) * 5/9;
+  return Math.round(convertedTemp * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  // 9/5 + 32
+  let convertedTemp = temp * 9/5 + 32;
+  return Math.round(convertedTemp * 10) / 10;
 };
 
 // Do not edit below this line
