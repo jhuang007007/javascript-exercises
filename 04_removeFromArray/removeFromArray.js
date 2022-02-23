5c19acae01bec74cc89a7f37d508ceb9db10722e
johnny <jhuang007007@gmail.com>
2022-02-22 23:46:29 -0500

    pass 4/7 tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+  let args = Array.prototype.slice.call(arguments);
+  args.sort.reverse;
+  for (let i = 1; i < args.length; i++) {
+    if (array.includes(args[i])) {
+      array.splice(args[i]-1, 1);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
