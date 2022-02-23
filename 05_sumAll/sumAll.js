bc6b12678795a493c5f170c0bb0e55956ff159ec
johnny <jhuang007007@gmail.com>
2022-02-23 13:04:23 -0500

    completed exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+  // sort the two integers so it is always ascending
+  const higherNum = Math.max(a,b);
+  const lowerNum = Math.min(a,b);
+  // initialize counter variable
+  let sum = 0;
+  // for loop starting from the lower number (inclusive) to the higher number (inclusive)
+  for (let i = lowerNum; i <= higherNum; i++) {
+    sum += i;
+  }
+  if (sum < 0 || typeof a === "string" || typeof b === "string" || typeof a === "object" || typeof b === "object") {
+    return "ERROR";
+  } else {
+    return sum;
+  }
+  
 };
 
 // Do not edit below this line
