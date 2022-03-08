784880db1b0c2d24e885787bf7b8b9942f1eca96
johnny <jhuang007007@gmail.com>
2022-03-08 13:54:08 -0500

    completed js exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  //filter out non-alphabetic
+  let word = string.toLowerCase().replace(/[^a-z]/g,""); 
+  //split string into array
+  let array = word.split('');
+  //create new array with order reversed
+  const reverse = array.slice().reverse();
+  //return true if the array is the same as the array in reverse, false if not
+  let result = (array.length == reverse.length) && array.every(function(element, i){
+    console.log(element);
+    console.log(reverse[i]);
+    return element === reverse[i];
+  })
+  return result;
 };
 
 // Do not edit below this line
