8bd2025ee215751694f20ece6532959480bfed18
johnny <jhuang007007@gmail.com>
2022-03-08 16:05:50 -0500

    completed js exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
-};
-
+const fibonacci = function(n) {
+  if (n < 0) return 'OOPS';
+  let i = 0;
+  let array = [];
+  let fib = 1;
+  array.push(fib);
+  while (i < n) {
+    array.push(fib);
+    fib += array[i];
+    i++;
+  } 
+  return array[n - 1];
+}
 // Do not edit below this line
 module.exports = fibonacci;
