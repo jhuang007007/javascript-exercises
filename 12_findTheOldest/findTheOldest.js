2dbc51b28ee8da396733b3e3e173731767d1fe42
johnny <jhuang007007@gmail.com>
2022-03-08 18:26:35 -0500

    completed js exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,13 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+  let ageIncluded = people.map(person => (
+    {
+      name: person.name,
+      age: (person.yearOfDeath === undefined) ? (2022 - person.yearOfBirth) : (person.yearOfDeath - person.yearOfBirth),
+    }
+  ));
+  console.table(ageIncluded);
+  return ageIncluded.reduce((prev, current) => (prev.age > current.age) ? prev : current);
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
